0a8588eede5cac6028a3cc77ad6e46067310873c
Vivek Nair <appusvivek1@gmail.com>
2017-10-16 13:12:44 +0530

    prep repo for heroku

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var express = require('express');
 //Create our app
 
 var app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(function(){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    }else{
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-        console.log('Express server running at port 3000');
+app.listen(PORT, function(){
+        console.log('Express server running at port '+ PORT);
 });
