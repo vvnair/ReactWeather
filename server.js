2221b7cd9d0e147176726f82f0c60ee3b30bd131
Vivek Nair <appusvivek1@gmail.com>
2017-10-16 13:33:21 +0530

    server js issue fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(){
+app.use(function(req, res, next){
     if(req.headers['x-forwarded-proto'] === 'http'){
         next();
     }else{
